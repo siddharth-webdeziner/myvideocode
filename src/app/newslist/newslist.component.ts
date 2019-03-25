036d5d5660c18efa5940f9a072dd3832465bf322
siddharth shahi <siddharth.shahi971@gmail.com>
2019-03-25 08:42:12 +0530

    news category

--- src/app/newslist/newslist.component.ts
+++ src/app/newslist/newslist.component.ts
@@ -8,27 +8,34 @@ import { PopulatenewsService } from '../services/populatenews.service';
   providers : [PopulatenewsService]
 })
 export class NewslistComponent implements OnInit {
-  newdata;
-  loadingPage;
+  newdata : any;
+  loadingPage : any;
+  searchNews: any;
   constructor(
     private populatenewsService: PopulatenewsService
   ) { }
 
   ngOnInit() {
     this.loadingPage = true;
+    this.searchNews = 'everything'
     this.newsLoaded();
   }
 
   newsLoaded(){
-    this.populatenewsService.getNewsData().subscribe(data => {
+    this.populatenewsService.getNewsData(this.searchNews).subscribe(data => {
       console.log("data : " ,data.articles)
       this.newdata = data.articles;
       setTimeout(_=>{this.loadingPage = false},1000);
     })
   }
 
-  newsDesc(i){
-    
+  getNews(){
+    console.log("searchText", this.searchNews)
+    this.populatenewsService.getNewsData(this.searchNews).subscribe(data => {
+      console.log("data : " ,data.articles)
+      this.newdata = data.articles;
+      setTimeout(_=>{this.loadingPage = false},1000);
+    })
   }
 
 }
--- src/app/services/populatenews.service.ts
+++ src/app/services/populatenews.service.ts
@@ -8,12 +8,12 @@ export class PopulatenewsService {
   url;
   currentNews;
   constructor(private http : Http) {
-    this.url  = 'https://newsapi.org/v2/everything?sources=the-times-of-india&apiKey=7245bbaa73a246deaebb09ecd5f2eeee';
+    this.url  = 'https://newsapi.org/v2/';
   }
 
-  getNewsData(){
+  getNewsData(getSelectedNews){
     //return this.http.get(this.url+"videolist", this.config).map(res => {
-    return this.http.get(this.url).map(res => {
+    return this.http.get(this.url+getSelectedNews+'?sources=the-times-of-india&apiKey=7245bbaa73a246deaebb09ecd5f2eeee').map(res => {
       this.currentNews = res.json();
       return res.json()
     })
